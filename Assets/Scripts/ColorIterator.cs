cef69f519d0524fb8e8718d9ced450b06f028c30
Peter Smith <pete.smith1988@gmail.com>
2017-01-21 13:10:41 +0000

    player creation and color switching v1

---
+++ Assets/Scripts/ColorIterator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorIterator
+{
+	readonly static Color[] Colors = new Color[]{
+		Color.red,
+		Color.blue,
+		Color.green
+	};
+
+	int Current = 0;
+
+	public ColorIterator()
+	{
+		Current = Random.Range (0, Colors.Length);
+	}
+
+	public Color NextColor()
+	{
+		// about to go out of range, circle back
+		if (Current == Colors.Length - 1)
+			Current = 0;
+		else
+			Current++;
+
+		return Colors[Current];
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData
+{
+	public string KeyOne;
+	public string KeyTwo;
+	public Color HairColor;
+	public Color SuitColor;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	enum PlayerState {
+		InMenu = 0,
+		OnWave,
+		InAir
+	};
+
+	PlayerState State;
+	PlayerData Data;
+
+	SpriteRenderer Hair;
+	SpriteRenderer Suit;
+
+	ColorIterator HairColors;
+	ColorIterator SuitColors;
+
+	void SetData(PlayerData _data)
+	{
+		Data = _data;
+	}
+		
+	void Start () 
+	{
+		Hair = transform.Find ("Hair").GetComponent<SpriteRenderer> ();
+		Suit = transform.Find ("Suit").GetComponent<SpriteRenderer> ();
+		HairColors = new ColorIterator ();
+		SuitColors = new ColorIterator ();
+		Hair.color = HairColors.NextColor();
+		Suit.color = SuitColors.NextColor();
+	}
+
+	void MenuInput()
+	{
+		if (Input.GetKeyDown (Data.KeyOne))
+		{
+			Hair.color = HairColors.NextColor();
+		}
+
+		if (Input.GetKeyDown (Data.KeyTwo))
+		{
+			Suit.color = SuitColors.NextColor();
+		}
+	}
+
+	void WaveInput()
+	{
+		// move
+	}
+
+	void AirInput()
+	{
+		// do tricks
+	}
+
+	void Update () 
+	{
+		switch (State)
+		{
+		case PlayerState.InMenu:
+			MenuInput ();
+			break;
+		case PlayerState.OnWave:
+			WaveInput ();
+			break;
+		case PlayerState.InAir:
+			AirInput ();
+			break;
+		}
+	}
+
+	static GameObject CachedPlayerPrefab = null;
+	public static PlayerController CreatePlayer(PlayerData _data)
+	{
+		if (CachedPlayerPrefab == null)
+		{
+			CachedPlayerPrefab = Resources.Load ("Player") as GameObject;
+		}
+
+		var created = Instantiate (CachedPlayerPrefab);
+		var controller = created.GetComponent<PlayerController> ();
+		controller.SetData (_data);
+		return controller;
+	}
+}
