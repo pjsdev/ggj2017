9bd274716de8eea5edd4b379be62fabdd58bf459
Tim Watts <tim@boxofclicks.com>
2017-02-03 11:36:08 +0000

    Added PlayerScoreCalculator and ScoreTrigger components

---
+++ Assets/Scripts/PlayerScoreCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreCalculator : MonoBehaviour
+{
+    public PlayerController controller;
+    public float DelayBeforeRepeatTriggerHit = 0.3f;
+
+    private WaitForSeconds WaitForTriggerHit;
+    private List<ScoreTrigger> RecentlyHitTriggers = new List<ScoreTrigger>();
+
+    void Awake()
+    {
+        WaitForTriggerHit = new WaitForSeconds(DelayBeforeRepeatTriggerHit);
+        if ( controller == null ) controller = GetComponentInParent<PlayerController>();
+    }
+
+    public void Hit( ScoreTrigger trigger )
+    {
+        if (trigger != null && !RecentlyHitTriggers.Contains(trigger) )
+        {
+            controller.Score += trigger.Score;
+            RecentlyHitTriggers.Add(trigger);
+            StartCoroutine(RemoveTriggerAfterDelay(trigger));
+
+            trigger.ShowScore(trigger.Score, controller.SuitColor);
+        }
+    }
+
+    private IEnumerator RemoveTriggerAfterDelay(ScoreTrigger trigger)
+    {
+        yield return WaitForTriggerHit;
+        if ( RecentlyHitTriggers.Contains(trigger))
+        {
+            RecentlyHitTriggers.Remove(trigger);
+        }
+    }
+
+    void OnDisable()
+    {
+        //Coroutines will stop firing so..
+        StopAllCoroutines();
+        RecentlyHitTriggers.Clear();
+    }
+}
--- Assets/Scripts/ScoreTrigger.cs
+++ Assets/Scripts/ScoreTrigger.cs
@@ -1,10 +1,62 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Collider2D))]
 public class ScoreTrigger : MonoBehaviour
 {
     public int Score = 100;
+    public float ShowScoreDuration = 1.0f;
+    public float FadeInDuration = 0.2f;
+    public float FadeOutDuration = 0.2f;
+    public Text TextPrefab;
 
+    private string PlayerTag = "Player";
+    private List<Text> ScoreTextFields = new List<Text>();
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if ( col.gameObject.CompareTag(PlayerTag))
+        {
+            PlayerScoreCalculator playerScorer = col.gameObject.GetComponentInParent<PlayerScoreCalculator>();
+
+            if (playerScorer != null) playerScorer.Hit(this);
+        }
+    }
+
+    public void ShowScore(int _Score, Color colour)
+    {
+        Text txt = GetTextField();
+        txt.text = _Score.ToString();
+        txt.color = new Color( colour.r, colour.g, colour.b, 0);
+        txt.DOFade(1f, FadeInDuration);
+        txt.DOFade(0f, FadeOutDuration).SetDelay(ShowScoreDuration);
+        txt.transform.DOLocalMoveY(txt.transform.position.y - 1f, FadeInDuration + FadeOutDuration + ShowScoreDuration);
+    }
+
+    // Checks pool for available textfield - creates one if needed
+    private Text GetTextField()
+    {
+        Text txt = null;
+        if ( ScoreTextFields.Count > 0 )
+        {
+            foreach(Text t in ScoreTextFields )
+            {
+                if ( t.color.a < 0.001f )
+                {
+                    txt = t;
+                    continue;
+                }
+            }
+        }
+        if ( txt == null )
+        {
+            txt = GameObject.Instantiate(TextPrefab.gameObject).GetComponent<Text>();
+            ScoreTextFields.Add(txt);
+        }
+
+        return txt;
+    }
 }
